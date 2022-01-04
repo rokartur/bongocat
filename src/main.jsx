939bd8cbfecb05b92d4a60a8c2a084b08ad03da6
Artur <rokartur520@gmail.com>
2022-01-04 19:34:31 +0100

    Added templates

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './assets/scss/DefaultDesktop.scss'
+import './assets/scss/Desktop.scss'
 import App from './App'
 
 ReactDOM.render(
